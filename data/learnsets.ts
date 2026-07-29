966a9a9e4d723a66722324ce25d6aebe18c8663f
ItzGray <masonmustonen@gmail.com>
2026-07-29 09:33:39 -0500

    Add moves and start on learnsets

--- data/learnsets.ts
+++ data/learnsets.ts
@@ -100439,4 +100439,27 @@ export const Learnsets: import('../sim/dex-species').LearnsetDataTable = {
 			{generation: 5, level: 47, moves: ["darkpulse", "flamethrower", "hyperbeam", "icebeam"]},
 		],
 	},
+	shinigami: {
+		learnset: {
+			earthquake: ["3L1"],
+			psychic: ["3L1"],
+			shadowswipe: ["3L1"],
+			crosschop: ["3L1"],
+			thrash: ["3L6"],
+		},
+	},
+	stitcher: {
+		learnset: {
+			thunderbolt: ["3L1"],
+			psychic: ["3L1"],
+			dragonclaw: ["3L1"],
+			icebeam: ["3L6"],
+		},
+	},
+	kingmadio: {
+		learnset: {
+			teleport: ["3L1"],
+			nightshade: ["3L100"],
+		},
+	},
 };
