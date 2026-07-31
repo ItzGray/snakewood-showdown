d8ea69c0b6101abdf23318919049e23a3d3bfb0c
ItzGray <masonmustonen@gmail.com>
2026-07-31 05:30:48 -0500

    Revert test on hiding a format + add some egg moves

--- config/formats.ts
+++ config/formats.ts
@@ -5334,8 +5334,6 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		gameType: 'doubles',
 		bestOfDefault: true,
 		searchShow: false,
-		debug: true,
-		battle: { trunc: Math.trunc },
 		ruleset: ['Flat Rules', '! Adjust Level Down', 'Max Level = 50'],
 		banlist: ['Tyranitar', 'Rotom', 'Judgment', 'Soul Dew'],
 	},
