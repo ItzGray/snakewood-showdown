452c058522f15452746c1504390e5c0d4b753324
ItzGray <masonmustonen@gmail.com>
2026-08-02 08:32:02 -0500

    Test client change

--- config/formats.ts
+++ config/formats.ts
@@ -26,7 +26,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		section: "Snakewood",
 	},
 	{
-		name: "[Gen 3] Ubers",
+		name: "[Snakewood] Ubers",
 		mod: 'gen3',
 		searchShow: false,
 		ruleset: ['Standard', 'Deoxys Camouflage Clause Mod', 'One Baton Pass Clause'],
