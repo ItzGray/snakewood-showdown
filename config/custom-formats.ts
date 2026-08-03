4747bf108b40a3096dcb956456093d37517127f6
ItzGray <masonmustonen@gmail.com>
2026-08-02 20:57:12 -0500

    Experiment with adding custom-formats.ts

---
+++ config/custom-formats.ts
@@ -0,0 +1,18 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: FormatList = [
+	// Snakewood NatDex
+	///////////////////////////////////////////////////////////////////
+
+	{
+		section: "Snakewood NatDex",
+	},
+	{
+		name: "[Gen 3] Ubers",
+		mod: 'gen3',
+		searchShow: false,
+		ruleset: ['Standard NatDex', 'Deoxys Camouflage Clause Mod', 'One Baton Pass Clause'],
+		banlist: ['Wobbuffet + Leftovers', 'Wynaut + Leftovers', 'Baton Pass'],
+	},
+];
