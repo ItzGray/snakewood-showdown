e5a30849321a65236191eacc5e8732440d8716f8
ItzGray <masonmustonen@gmail.com>
2026-08-02 20:59:16 -0500

    Experiment with adding NatDex

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -9,7 +9,7 @@ export const Formats: FormatList = [
 		section: "Snakewood NatDex",
 	},
 	{
-		name: "[Gen 3] Ubers",
+		name: "[Gen 3] National Dex Ubers",
 		mod: 'gen3',
 		searchShow: false,
 		ruleset: ['Standard NatDex', 'Deoxys Camouflage Clause Mod', 'One Baton Pass Clause'],
