b3f37e568a3351f5da321600de3411598f9030ec
ItzGray <masonmustonen@gmail.com>
2026-08-24 08:15:15 -0500

    Temporary change for testing purposes

--- data/random-battles/gen3/teams.ts
+++ data/random-battles/gen3/teams.ts
@@ -783,9 +783,9 @@ export class RandomGen3Teams extends RandomGen4Teams {
 			if (species.id === 'ditto') this.battleHasDitto = true;
 		}
 
-		if (pokemon.length < this.maxTeamSize && !isMonotype && !this.forceMonotype && pokemon.length < 12) {
-			throw new Error(`Could not build a random team for ${this.format} (seed=${seed})`);
-		}
+		//if (pokemon.length < this.maxTeamSize && !isMonotype && !this.forceMonotype && pokemon.length < 12) {
+		//	throw new Error(`Could not build a random team for ${this.format} (seed=${seed})`);
+		//}
 
 		return pokemon;
 	}
